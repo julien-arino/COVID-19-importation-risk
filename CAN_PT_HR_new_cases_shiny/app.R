a6492391efea2b646a995eeec9afce40ca7b2f0c
Ubuntu <ubuntu@ip-172-31-5-132.ca-central-1.compute.internal>
2020-08-20 00:02:04 +0000

    Added some stuff

--- CAN_PT_HR_new_cases_shiny/app.R
+++ CAN_PT_HR_new_cases_shiny/app.R
@@ -58,6 +58,22 @@ ui <- fluidPage(
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
+
+    set_directories = function() {
+    ### SET_DIRECTORIES
+# Need a special ad hoc version here because we are running in Shiny server
+# Create list to store the information
+DIRS = list()
+# Set code directory
+DIRS$CODE = "/home/ubuntu/github/covid-19-importation-risk"
+# Directories outside Github repo: DATA, others perhaps
+DIRS$OUTSIDE = "/home/ubuntu"
+# A few directories to store stuff
+DIRS$OUTPUT = sprintf("%s/OUTPUT", DIRS$OUTSIDE)
+DIRS$FIGS = sprintf("%s/FIGS", DIRS$OUTSIDE)
+DIRS$COVID_DATA = sprintf("%s/DATA", DIRS$OUTSIDE)
+return(DIRS)
+    }
     # Reactive expression to generate the requested distribution ----
     # This is called whenever the inputs change. The output functions
     # defined below then use the value computed from this expression
--- CAN_PT_HR_new_cases_shiny/set_directories.R
+++ CAN_PT_HR_new_cases_shiny/set_directories.R
@@ -1,26 +1,12 @@
 ### SET_DIRECTORIES
 
-# Need a special version here because we are running in Shiny server
+# Need a special ad hoc version here because we are running in Shiny server
 # Create list to store the information
 DIRS = list()
 # Set code directory
-DIRS$CODE = here::here()
+DIRS$CODE = "/home/ubuntu/github/covid-19-importation-risk"
 # Directories outside Github repo: DATA, others perhaps
-if (Sys.info()["sysname"] == "Windows") {
-  # Assuming your data is on D. Further tailor to your situation using the
-  # output of Sys.info()["nodename"], if need be
-  DIRS$OUTSIDE = "D:/Documents"
-} else if (Sys.info()["nodename"] %in% c("jarino-compute1",
-                                         "jarino-compute2",
-                                         "jarino-compute3",
-                                         "jarino-compute4",
-                                         "raspberry")) {
-  # Example of a nonstandard unix config: my home compute cluster's shared NAS drive
-  DIRS$OUTSIDE = "/mnt/NFS_LaCie"
-} else {
-  # On unix, typically this would work. Adapt to your setting
-  DIRS$OUTSIDE = sprintf("/home/%s", Sys.info()["user"])
-}
+DIRS$OUTSIDE = "/home/ubuntu"
 # A few directories to store stuff
 DIRS$OUTPUT = sprintf("%s/OUTPUT", DIRS$OUTSIDE)
 DIRS$FIGS = sprintf("%s/FIGS", DIRS$OUTSIDE)
